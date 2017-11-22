2a1a758cbc75ed2eb7f889e7de0c88465e8e42f5
AB3000 <31480939+AB3000@users.noreply.github.com>
2017-11-22 16:52:26 -0500

    Instructions Working

---
+++ Tree Game/Assets/Scripts/Fader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour {
+
+	public Texture2D fadeOutTexture; //texture over the scene
+	public float fadeSpeed = 0.8f; //fade speed
+
+	private int drawDepth = -1000; //order in game
+	private float alpha = 1.0f; 
+	private int fadeDir = -1; //fade in = 1, fade out = -1
+	// Use this for initialization
+
+
+	void OnGUI(){
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		alpha = Mathf.Clamp01 (alpha);
+		GUI.color = new Color (GUI.color.r,GUI.color.g,GUI.color.b ); 
+		GUI.depth = drawDepth; //black goes on top
+		GUI.DrawTexture(new Rect(0,0, Screen.width, Screen.height) ,fadeOutTexture); //fits entire screen
+	}
+
+	public float BeginFade(int direction){
+		fadeDir = direction;
+		return (fadeSpeed); 
+	}
+
+	void OnLevelWasLoaded(){
+		alpha = 1;
+		BeginFade (-1); 
+	}
+}
---
+++ Tree Game/Assets/Scripts/starter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starter : MonoBehaviour {
+
+	public string scene;
+	//WORKS
+	public void ChangeScenes(string scene){
+		Application.LoadLevel (scene);
+
+		//float fadeTime = GameObject.Find ("treasurechest").GetComponent<Fader> ().BeginFade (1);
+
+	}
+
+	/*IEnumerator ChangeScenes(){
+		float fadeTime = GameObject.Find ("treasurechest").GetComponent<Fader> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		Application.LoadLevel (scene);
+		
+	}*/
+
+
+
+
+	// Use this for initialization
+
+}
