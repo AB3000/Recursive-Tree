a548e4cbeb9a58505948c17c97b5808f3c76b480
Kaiyang Zhao <magickaiyang@users.noreply.github.com>
2017-11-23 14:31:54 -0500

    highlight and node indicator

--- Tree Game/Assets/Recurse.cs
+++ Tree Game/Assets/Recurse.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
+using UnityEngine.UI;
 
 public class Recurse : MonoBehaviour
 {
     public BallLerp bl;
     public NodeScript root;
+    public Text current;
 
     // Use this for initialization
     void Start()
@@ -22,7 +23,10 @@ public class Recurse : MonoBehaviour
 
     IEnumerator Traverse_1(NodeScript n)  //left then right
     {
-        Debug.Log(n);
+        UpdateCurrent(n);
+
+        yield return new WaitForSeconds(.5f);
+
         if (n.leftChild == null)
         {
             yield break;
@@ -32,6 +36,8 @@ public class Recurse : MonoBehaviour
         yield return new WaitForSeconds(1f);
 
         yield return StartCoroutine(Traverse_1(n.leftChild));
+
+        UpdateCurrent(n);
 
         yield return new WaitForSeconds(1f);
 
@@ -49,16 +55,21 @@ public class Recurse : MonoBehaviour
 
         yield return StartCoroutine(Traverse_1(n.rightChild));
 
-        yield return new WaitForSeconds(1f);
+
+        UpdateCurrent(n);
 
         bl.GoBack();
     }
 
     IEnumerator Traverse_2(NodeScript n)  //right then left
     {
+        UpdateCurrent(n);
+
+        yield return new WaitForSeconds(.5f);
+
         if (n.rightChild == null)
         {
-            yield return null;
+            yield break;
         }
         bl.GoRight();
 
@@ -66,11 +77,18 @@ public class Recurse : MonoBehaviour
 
         yield return StartCoroutine(Traverse_2(n.rightChild));
 
+        UpdateCurrent(n);
+
         yield return new WaitForSeconds(1f);
 
         bl.GoBack();
 
         yield return new WaitForSeconds(1f);
+
+        if (n.leftChild == null)
+        {
+            yield break;
+        }
 
         bl.GoLeft();
 
@@ -78,9 +96,14 @@ public class Recurse : MonoBehaviour
 
         yield return StartCoroutine(Traverse_2(n.leftChild));
 
-        yield return new WaitForSeconds(1f);
+        UpdateCurrent(n);
 
         bl.GoBack();
+    }
+
+    public void UpdateCurrent(NodeScript n)
+    {
+        current.text = "Current Node: " + n.gameObject.name;
     }
 
     public void OnClick()
@@ -89,11 +112,13 @@ public class Recurse : MonoBehaviour
         if (Inventory.sequence.Equals("TextA TextB "))
         {
             print("mode 1");
+
             StartCoroutine(Traverse_1(root));
         }
         else if (Inventory.sequence.Equals("TextB TextA "))
         {
             print("mode 2");
+
             StartCoroutine(Traverse_2(root));
         }
     }
--- Tree Game/Assets/Scripts/BallLerp.cs
+++ Tree Game/Assets/Scripts/BallLerp.cs
@@ -1,113 +1,136 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BallLerp : MonoBehaviour {
+public class BallLerp : MonoBehaviour
+{
 
-	public NodeScript head;
-	//public Transform startMarker;
-	public Transform endMarker;
-	public float speed = 1F;
-	private float startTime;
-	private float journeyLength;
-	private float distCovered;
+    public NodeScript head;
+    //public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 1F;
+    private float startTime;
+    private float journeyLength;
+    private float distCovered;
+
+    public Image recurseL;
+    public Image recurseR;
+
+    //private float direction;
+    Vector3 dire;
+    //int count;
+    void Start()
+    {
+        startTime = Time.time;
+        //startMarker = this.transform;
+        //endMarker = new Transform();
+        endMarker.position = head.transform.position;
+        journeyLength = Vector3.Distance(transform.position, endMarker.position);
+
+    }
+    void Update()
+    {
 
 
+        distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        //transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+        //startMarker.position = transform.position;
+        transform.position = Vector3.Lerp(transform.position, endMarker.position, fracJourney);
 
-	//private float direction;
-	Vector3 dire;
-	//int count;
-	void Start() {
-		startTime = Time.time;
-		//startMarker = this.transform;
-		//endMarker = new Transform();
-		endMarker.position = head.transform.position;
-		journeyLength = Vector3.Distance(transform.position, endMarker.position);
+        endMarker.position = head.transform.position;
 
-	}
-	void Update() {
-		
+        if (head.rightChild == null)
+        {
+            Debug.Log("You are at the end!");
+            return;
+        }
 
-		distCovered = (Time.time - startTime) * speed;
-		float fracJourney = distCovered / journeyLength;
-		//transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
-		//startMarker.position = transform.position;
-		transform.position = Vector3.Lerp(transform.position, endMarker.position, fracJourney);
-
-		endMarker.position = head.transform.position;
-
-		if (head.rightChild == null) {
-			Debug.Log ("You are at the end!");
-			return;
-		}
-
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			/*startTime = Time.time;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            /*startTime = Time.time;
 			distCovered = 0;
 			head = head.leftChild;*/
-			GoLeft ();
-			//Debug.Log ("This is the head position " + head.transform.position);
-			//endMarker.position = head.transform.position;
+            GoLeft();
+            //Debug.Log ("This is the head position " + head.transform.position);
+            //endMarker.position = head.transform.position;
 
-		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			/*startTime = Time.time;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            /*startTime = Time.time;
 			distCovered = 0;
 			head = head.rightChild;*/
-			GoRight ();
-			//endMarker = posit
-		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			GoBack ();
-			Debug.Log ("Head is now " + head + "with position" + head.transform.position);
+            GoRight();
+            //endMarker = posit
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GoBack();
+            Debug.Log("Head is now " + head + "with position" + head.transform.position);
 
-		}
+        }
 
-		//Debug.Log ("The position of the head " + head.transform.position);
+        //Debug.Log ("The position of the head " + head.transform.position);
 
-		//endMarker.position = head.transform.position;
-
-
-	}
-
-
-	public bool GoLeft(){
-		startTime = Time.time;
-		distCovered = 0;
-		if (head.leftChild == null)
-			return false;
-		else {
-			head = head.leftChild;
-			return true;
-		}
+        //endMarker.position = head.transform.position;
 
 
-	}
+    }
 
-	public bool GoRight(){
-		startTime = Time.time;
-		distCovered = 0;
-		if (head.rightChild == null)
-			return false;
-		else {
-			head = head.rightChild;
-			return true;
-		}
-		//head = head.rightChild;
 
-	}
+    public bool GoLeft()
+    {
+        recurseL.color = Color.yellow;
+        recurseR.color = Color.white;
 
-	public bool GoBack(){
-		startTime = Time.time;
-		distCovered = 0;
-		if (head.head == null)
-			return false;
-		else {
-			Debug.Log ("head pos: " + head.head.transform.position);
-			head = head.head;
-			return true;
-		}
-		//head = head.head;
-	}
-	/*void OnGUI(){
+        startTime = Time.time;
+        distCovered = 0;
+        if (head.leftChild == null)
+            return false;
+        else
+        {
+            head = head.leftChild;
+            return true;
+        }
+
+
+    }
+
+    public bool GoRight()
+    {
+        recurseL.color = Color.white;
+        recurseR.color = Color.yellow;
+
+        startTime = Time.time;
+        distCovered = 0;
+        if (head.rightChild == null)
+            return false;
+        else
+        {
+            head = head.rightChild;
+            return true;
+        }
+        //head = head.rightChild;
+
+    }
+
+    public bool GoBack()
+    {
+        startTime = Time.time;
+        distCovered = 0;
+        if (head.head == null)
+            return false;
+        else
+        {
+            Debug.Log("head pos: " + head.head.transform.position);
+            head = head.head;
+            return true;
+        }
+        //head = head.head;
+    }
+    /*void OnGUI(){
 		GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 200f, 200f), "dsadsadsa");
 	}*/
 }
